bd7d847ee7485474dca82265cf8a57513adbfc95
AMMARE0 <162882326+AMMARE0@users.noreply.github.com>
2026-03-12 14:43:45 +1100

    Update log_parser.py

--- parsers/log_parser.py
+++ parsers/log_parser.py
@@ -1,18 +1,26 @@
+import re
+from collections import defaultdict
+
+
 def parse_failed_ssh_attempts(log_file):
-    failed_attempts = []
+    ip_attempts = defaultdict(int)
 
     with open(log_file, "r") as file:
         for line in file:
             if "Failed password" in line:
-                failed_attempts.append(line.strip())
+                match = re.search(r'from (\d+\.\d+\.\d+\.\d+)', line)
+                if match:
+                    ip = match.group(1)
+                    ip_attempts[ip] += 1
 
-    return failed_attempts
+    return ip_attempts
 
 
 if __name__ == "__main__":
     log_path = "sample_logs/auth_sample.log"
-    failed_logs = parse_failed_ssh_attempts(log_path)
+    failed_attempts = parse_failed_ssh_attempts(log_path)
 
-    print("Failed SSH Login Attempts:\n")
-    for log in failed_logs:
-        print(log)
+    print("SSH Failed Login Attempts by IP:\n")
+
+    for ip, count in failed_attempts.items():
+        print(f"{ip} -> {count} failed attempts")

26fc3470b5f061fc302bfe2c1dfb2c98e4585e63
AMMARE0 <162882326+AMMARE0@users.noreply.github.com>
2026-03-12 14:24:22 +1100

    Update log_parser.py

--- parsers/log_parser.py
+++ parsers/log_parser.py
@@ -1,1 +1,18 @@
+def parse_failed_ssh_attempts(log_file):
+    failed_attempts = []
 
+    with open(log_file, "r") as file:
+        for line in file:
+            if "Failed password" in line:
+                failed_attempts.append(line.strip())
+
+    return failed_attempts
+
+
+if __name__ == "__main__":
+    log_path = "sample_logs/auth_sample.log"
+    failed_logs = parse_failed_ssh_attempts(log_path)
+
+    print("Failed SSH Login Attempts:\n")
+    for log in failed_logs:
+        print(log)
